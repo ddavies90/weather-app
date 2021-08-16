ace2ad4977fd61ba0d61efd82167f1438300cd57
David Davies <d.davies90@hotmail.co.uk>
2021-08-16 07:45:38 +0100

    Created ForecastSummaries component

---
+++ src/components/ForecastSummaries.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/ForecastSummaries.css";
+import ForecastSummary from "./ForecastSummary";
+
+const ForecastSummaries = ({ forecasts }) => {
+  return (
+    <div className="forecast-summaries">
+      {forecasts.map((forecast) => {
+        const { id, date, temperature, icon, description } = forecast;
+        return (
+          <ForecastSummary
+            key={id}
+            id={id}
+            date={date}
+            icon={icon}
+            temperature={temperature}
+            description={description}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+ForecastSummaries.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      date: PropTypes.number.isRequired,
+      icon: PropTypes.string.isRequired,
+      temperature: PropTypes.shape({
+        min: PropTypes.number.isRequired,
+        max: PropTypes.number.isRequired
+      }),
+      description: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+};
+
+export default ForecastSummaries;
