fad7c1ac6e046f11ff33ba59c1c142dc056e6da7
David Davies <d.davies90@hotmail.co.uk>
2021-08-18 08:24:35 +0100

    Hooked up to owm API - new state required for location and forecasts. Used a useEffect to update state in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,37 @@
 import "../styles/App.css";
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
+import getForecasts from "../requests/getForecasts";
 
-const App = ({ location, forecasts }) => {
-  const { city, country } = location;
-  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
-  const selectedForecast = forecasts.find(
-    (forecast) => forecast.date === selectedDate
-  );
+const App = () => {
+  const [forecasts, setForecasts] = useState([]);
+  const [location, setLocation] = useState({ city: "", country: "" });
+  const [selectedDate, setSelectedDate] = useState(0);
+
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
   };
 
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
+
+  useEffect(() => {
+    getForecasts(setSelectedDate, setLocation, setForecasts);
+  }, []);
+
   return (
     <div className="weather-app">
-      <LocationDetails city={city} country={country} />
+      <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
       />
-      <ForecastDetails forecast={selectedForecast} />
+      {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
     </div>
   );
-};
-
-App.propTypes = {
-  forecasts: PropTypes.arrayOf(
-    PropTypes.shape({
-      date: PropTypes.number,
-      description: PropTypes.string,
-      icon: PropTypes.string,
-      temperature: PropTypes.shape({
-        max: PropTypes.number,
-        min: PropTypes.number
-      })
-    })
-  ).isRequired,
-  location: PropTypes.shape({
-    city: PropTypes.string,
-    country: PropTypes.string
-  }).isRequired
 };
 
 export default App;
