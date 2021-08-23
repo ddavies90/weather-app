df17bd0c5c40194e6c122236c9d34b44eb5605b7
David Davies <d.davies90@hotmail.co.uk>
2021-08-23 08:22:15 +0100

    Added searchform component to app output. Added state function to keep track of the searched value in the searchform.

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecasts from "../requests/getForecasts";
+import SearchForm from "./SearchForm";
 
 const App = () => {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
+  const [searchText, setSearchText] = useState("");
 
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
@@ -17,6 +19,13 @@ const App = () => {
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
   );
+
+  const handleCitySearch = async () => {
+    const response = await getForecasts(searchText);
+    setForecasts(response.forecasts);
+    setLocation(response.location);
+    setSelectedDate(response.forecasts[0].date);
+  };
 
   useEffect(async () => {
     const response = await getForecasts();
@@ -28,6 +37,11 @@ const App = () => {
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm
+        searchValue={searchText}
+        setSearchValue={setSearchText}
+        citySearchFunc={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
