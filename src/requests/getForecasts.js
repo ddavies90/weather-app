c30cc4772fd2dc40c1042d1c7cc5f19b19341f24
David Davies <30637519+ddavies90@users.noreply.github.com>
2021-08-29 02:51:20 +0100

    Update getForecasts.js

--- src/requests/getForecasts.js
+++ src/requests/getForecasts.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const getForecasts = async (query) => {
-  const geocodingEndpoint = `http://api.positionstack.com/v1/forward?access_key=34df3fd424fa176bed1bcd96f84856d6&limit=1&query=${query}`;
+  const geocodingEndpoint = `http://api.positionstack.com/v1/forward?access_key=${YOUR_API_KEY}&limit=1&query=${query}`;
   const { data: geoData } = await axios.get(geocodingEndpoint);
   const {
     data: [{ latitude, longitude, name, country_code: countryCode }]
   } = geoData;
 
-  const owmEndpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely,hourly,alerts&appid=a8cd1915c0987e8a70dcda887caa65e5
+  const owmEndpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely,hourly,alerts&appid=${OWM_API_KEY}
   `;
 
   const { data } = await axios.get(owmEndpoint);
