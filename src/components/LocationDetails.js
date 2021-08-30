c6abacff61c941d9f926b60ade101b9acfea6b74
David Davies <d.davies90@hotmail.co.uk>
2021-08-30 22:05:30 +0100

    Added error to incoming props. Added conditional rendering to display either a paragraph with error message if error is true, or the location header if city is true. Added error to proptypes with default proptype of empty string. Returned null at the end of the function as es-lint was complaining about no return statement at the end of an arrow function - what should I have done instead?

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -2,20 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../styles/LocationDetails.css";
 
+// eslint-disable-next-line consistent-return
 const LocationDetails = (props) => {
-  const { city, country } = props;
-  return (
-    <h1 className="location-details">
-      {`${city},`}
-      <br />
-      {`${country}`}
-    </h1>
-  );
+  const { city, country, error } = props;
+  if (error) {
+    return <p className="error-message">{error}</p>;
+  }
+  if (city) {
+    return (
+      <h1 className="location-details" data-testid="location-header">
+        {`${city},`}
+        <br />
+        {`${country}`}
+      </h1>
+    );
+  }
+  return null;
 };
 
 LocationDetails.propTypes = {
   city: PropTypes.string.isRequired,
-  country: PropTypes.string.isRequired
+  country: PropTypes.string.isRequired,
+  error: PropTypes.string
+};
+
+LocationDetails.defaultProps = {
+  error: ""
 };
 
 export default LocationDetails;
